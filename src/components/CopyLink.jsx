be1defbe4f337357cbbf474ee3158b4f2f736393
Silvia <silviajcn@gmail.com>
2022-07-07 22:18:43 -0500

    copy to clipboard funcional

--- src/components/CopyLink.jsx
+++ src/components/CopyLink.jsx
@@ -1,17 +1,30 @@
 import { FaLink } from "react-icons/fa";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Toaster, toast } from 'react-hot-toast';
+//import { useState } from "react";
 
 const notify = () => toast('Link copied to clipboard!', {position: 'top-center'});
 
-const CopyLink = ({ url }) => {
+const CopyLink = ({ link }) => {
+
+    //Otra forma de hacer la notificacion sin el toaster. Con el useState:
+    //const [copied, setCopied] = useState(false);
+
     return (
         <>
-            <CopyToClipboard text={url}>
-                <button onClick={notify} className="link-icon">
-                    <FaLink className="link-icon"/>
+            <CopyToClipboard text={link}>
+                <button
+                    //onClick={() => setCopied(true)}
+                    onClick={notify} 
+                    className="link-icon"
+                >
+                    <FaLink className="link-icon" />
                 </button>
             </CopyToClipboard>
+
+            {/* <p style={{ textAlign: "center" }}>
+                {copied && <span>Link copiado</span>}
+            </p> */}
 
             <Toaster toastOptions={{className: 'toaster'}} />
         </>
